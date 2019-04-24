1dcc6ae5d3abca1eaa66b14ce45066fbb5864f96
Igor Prokhorov <iprokhorovster@gmail.com>
2019-04-24 22:07:40 +0300

    some

--- src/games/card/PokerChance.java
+++ src/games/card/PokerChance.java
@@ -8,34 +8,10 @@ public class PokerChance {
     public static void main(String[] args) {
 
         ArrayList<Card> deck = DeckOfCards.getInstance().getShuffledDeck();
+        for (Card c : deck) {
+            System.out.println(c.getIdx() + "\t" + c.getCardValue() + c.getSuit());
+        }
 
-        Card myFirstCard = deck.get(0);
-        Card mySecondCard = deck.get(1);
-        Card cOne = deck.get(2);
-        Card cTwo = deck.get(3);
-        Card cThree = deck.get(4);
-        Card cFour = deck.get(5);
-        Card cFive = deck.get(6);
-
-        // Flash Royal
-
-        // Street Flash
-
-        // Care
-
-        // Full house
-
-        // Flash
-
-        // Street
-
-        // Set
-
-        // 2 pairs
-
-        // Pair
-
-        // High card
 
     }
 
