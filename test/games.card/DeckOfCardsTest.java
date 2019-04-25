b1c10c2d80e037725ccd2ecb3649ff1f1a3ef886
Igor Prokhorov <iprokhorovster@gmail.com>
2019-04-25 22:21:13 +0300

    try tests

--- test/games.card/DeckOfCardsTest.java
+++ test/games.card/DeckOfCardsTest.java
@@ -1,11 +1,34 @@
 package games.card;
 
+import org.junit.Assert;
 import org.junit.Test;
+
+import java.util.ArrayList;
+import java.util.Collections;
+
+
+import static games.card.Card.cardComparator;
 
 public class DeckOfCardsTest {
 
+    /**
+     * Test if id's of deck are the same for couple instances
+     */
     @Test
-    public void test1(){
+    public void testDeck() {
+        ArrayList<Card> deck1 = DeckOfCards.getInstance().getDeck();
+        ArrayList<Card> deck2 = DeckOfCards.getInstance().getDeck();
+        ArrayList<Card> deck3 = DeckOfCards.getInstance().getShuffledDeck();
+        ArrayList<Card> deck4 = DeckOfCards.getInstance().getShuffledDeck();
+
+
+//        Collections.sort(deck3, cardComparator);
+//        deck3.add(new Card("A", "♤"));
+        for (Card c: deck3
+             ) {
+            System.out.println(c.getIdx());
+        }
+
 
     }
 }
