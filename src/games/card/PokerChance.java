1c6c05c95160325c214a6b05fcb1e363e0462970
Игорь Прохоров <iprokhorov@mobicont.ru>
2019-06-07 08:07:42 +0300

    go frwd

--- src/games/card/PokerChance.java
+++ src/games/card/PokerChance.java
@@ -47,8 +47,8 @@ public class PokerChance {
             }
             // Get 5 random cards
             deal = DeckOfCards.getShuffledDeckExclude(exclude).subList(0, 5);
-            System.out.println("Deal number " + i + " is:");
-            DeckOfCards.printCards(deal);
+//            System.out.println("Deal number " + i + " is:");
+//            DeckOfCards.printCards(deal);
             // add first hand and deal
             firstHandDeal.addAll(firstHand);
             firstHandDeal.addAll(deal);
@@ -69,23 +69,23 @@ public class PokerChance {
 //            System.out.println(firstE + " " + secondE);
             if (firstE.getPriority() < secondE.getPriority()) {
                 // first hand win
-                System.out.println("First hand win! With " + firstE);
+//                System.out.println("First hand win! With " + firstE);
                 wins[0][0]++;
                 wins[1][1]++;
             } else if (firstE.getPriority() > secondE.getPriority()) {
                 // second hand win
-                System.out.println("Second hand win! With " + secondE);
+//                System.out.println("Second hand win! With " + secondE);
                 wins[0][1]++;
                 wins[1][0]++;
             } else {
                 // draw
-                System.out.println("Draw! " + firstE + " " + secondE);
+//                System.out.println("Draw! " + firstE + " " + secondE);
                 wins[0][2]++;
                 wins[1][2]++;
             }
             // fill HashMap
             mapFirstHand.replace(firstE, mapFirstHand.get(firstE) + 1);
-            mapSecondHand.replace(secondE, mapFirstHand.get(secondE) + 1);
+            mapSecondHand.replace(secondE, mapSecondHand.get(secondE) + 1);
 
             // clear hands deal
             deal.clear();
@@ -115,84 +115,6 @@ public class PokerChance {
                    mapFirstHand.get(pokerComboEnum),
                    mapSecondHand.get(pokerComboEnum));
         }
-
-
-/*        ArrayList<Card> example = new ArrayList<>();
-        example.add(DeckOfCards.getCardByValSuit("A", "♠"));
-        example.add(DeckOfCards.getCardByValSuit("A", "♣"));
-//        example.add(DeckOfCards.getCardByValSuit("K", "♦"));
-//        example.add(DeckOfCards.getCardByValSuit("Q", "♦"));
-        int
-                cntFlashRoyal = 0,
-                cntStreetFlash = 0,
-                cntCare = 0,
-                cntFullHouse = 0,
-                cntFlash = 0,
-                cntStreet = 0,
-                cntSet = 0,
-                cntPairs = 0,
-                cntPair = 0,
-                cntHighCard = 0;
-//        ArrayList<Card> deal = new ArrayList<>();
-        int count = 10000000;
-        for (int i = 0; i < count; i++) {
-            if (i == count / 4) System.out.println("25% done");
-            if (i == count / 2) System.out.println("50% done");
-            if (i == count / 2 + count / 4) System.out.println("75% done");
-            deal.addAll(example);
-            deal.addAll(DeckOfCards.getShuffledDeckExclude(example).subList(0, 5));
-            switch (PokerComboGetter.getComboEnum(deal)) {
-                case FLASHROYAL:
-                    cntFlashRoyal++;
-                    break;
-                case STREETFLASH:
-                    cntStreetFlash++;
-                    break;
-                case CARE:
-                    cntCare++;
-                    break;
-                case FULLHOUSE:
-                    cntFullHouse++;
-                    break;
-                case FLASH:
-                    cntFlash++;
-                    break;
-                case STREET:
-                    cntStreet++;
-                    break;
-                case SET:
-                    cntSet++;
-                    break;
-                case PAIRS:
-                    cntPairs++;
-                    break;
-                case PAIR:
-                    cntPair++;
-                    break;
-                case HIGHCARD:
-                    cntHighCard++;
-                    break;
-                default:
-                    System.out.println("WTF!!!");
-                    System.exit(1);
-
-            }
-            deal.clear();
-        }
-        System.out.println("Flash Royal  = " + 100.0 * cntFlashRoyal / count + "%");
-        System.out.println("Street Flash = " + 100.0 * cntStreetFlash / count + "%");
-        System.out.println("Care         = " + 100.0 * cntCare / count + "%");
-        System.out.println("Full House   = " + 100.0 * cntFullHouse / count + "%");
-        System.out.println("Flash        = " + 100.0 * cntFlash / count + "%");
-        System.out.println("Street       = " + 100.0 * cntStreet / count + "%");
-        System.out.println("Set          = " + 100.0 * cntSet / count + "%");
-        System.out.println("Pairs        = " + 100.0 * cntPairs / count + "%");
-        System.out.println("Pair         = " + 100.0 * cntPair / count + "%");
-        System.out.println("High Card    = " + 100.0 * cntHighCard / count + "%");
-        System.out.println("Total        = " + (cntFlashRoyal +
-                cntStreetFlash + cntCare + cntFullHouse + cntFlash + cntStreet +
-                cntSet + cntPairs + cntPair + cntHighCard) * 100.0 / count + "%");
-                */
     }
 
     /**
