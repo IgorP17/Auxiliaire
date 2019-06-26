dbb3b8dbba97934cb7ed311b34faef225421fc0f
Igor Prokhorov <iprokhorovster@gmail.com>
2019-06-26 22:22:27 +0300

    digit recognition hyperskill start

--- src/digit/recognition/Sample.java
+++ src/digit/recognition/Sample.java
@@ -1,4 +1,15 @@
 package digit.recognition;
 
+import java.util.Scanner;
+
 public class Sample {
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        int year = scanner.nextInt();
+        boolean result =
+
+        System.out.println();
+    }
 }
