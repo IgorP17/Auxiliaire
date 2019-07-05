5991a46359917e62d56f14a4ec4e423a6fb0c8fe
Prokhorov <prokhorov@bpcbt.com>
2019-07-05 10:32:00 +0300

    Start stage 2

--- src/digit/recognition/DigitRecognitionTestSmth.java
+++ src/digit/recognition/DigitRecognitionTestSmth.java
@@ -1,19 +1,29 @@
 package digit.recognition;
 
+import java.util.Arrays;
+import java.util.List;
 import java.util.Scanner;
+import java.util.stream.Collectors;
 
 public class DigitRecognitionTestSmth {
   public static void main(String[] args) {
     Scanner scanner = new Scanner(System.in);
-    int a = scanner.nextInt();
-    int b = scanner.nextInt();
-    int c = scanner.nextInt();
-    int d = scanner.nextInt();
-
-    for (int i = 0; i <= 1000; i++) {
-      if (a * i * i * i + b * i * i + c * i + d == 0){
-        System.out.println(i);
-      }
-    }
+    int[] array = Arrays.stream(scanner.nextLine().split(" "))
+            .mapToInt(Integer::parseInt)
+            .toArray();
+    int[] result = getFirstAndLast(array);
+    Arrays.stream(result).forEach(e -> System.out.print(e + " "));
   }
+
+  private static int[] getFirstAndLast(int[] array) {
+    return new int[]{array[0], array[array.length - 1]};
+  }
+
+
 }
+
+/*  Get massive
+    long[] array = Arrays.stream(scanner.nextLine().split(" "))
+            .mapToLong(Long::parseLong)
+            .toArray();
+ */
