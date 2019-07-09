7fcb958de68fa7861d6acd7466a76e064bb6494f
Игорь Прохоров <iprokhorov@mobicont.ru>
2019-07-09 07:39:25 +0300

    digit recognition state3

--- src/digit/recognition/DigitRecognitionTestSmth.java
+++ src/digit/recognition/DigitRecognitionTestSmth.java
@@ -1,16 +1,23 @@
 package digit.recognition;
 
 import java.util.Locale;
+import java.util.Random;
 import java.util.Scanner;
 
 public class DigitRecognitionTestSmth {
-  public static void main(String[] args) {
-    Scanner scanner = new Scanner(System.in).useLocale(Locale.US);
-    double c = scanner.nextDouble();
-    double f = c * 1.8 + 32;
-    System.out.println(f);
-  }
-
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in).useLocale(Locale.US);
+        int n = scanner.nextInt();
+        int a = scanner.nextInt();
+        int b = scanner.nextInt();
+        Random random = new Random();
+        random.setSeed(a + b);
+        int sum = 0;
+        for (int i = 0; i < n; i++) {
+            sum += random.nextInt(b - a + 1) + a;
+        }
+        System.out.println(sum);
+    }
 
 
 }
