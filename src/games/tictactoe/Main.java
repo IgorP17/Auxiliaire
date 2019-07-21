b9a16a1668a3a4f1dc0ad37c7fe606b73dce8b65
Игорь Прохоров <iprokhorov@mobicont.ru>
2019-07-21 18:19:29 +0300

    tic tac toe go medium :-)

--- src/games/tictactoe/Main.java
+++ src/games/tictactoe/Main.java
@@ -11,12 +11,6 @@ public class Main {
 
     public static void main(String[] args) {
 
-//        System.out.print("Enter cells: ");
-//        String s = scanner.nextLine();
-//        s = s.replace("\"", "");
-//        String[] f = s.split("");
-//        printField(f);
-
         String sState;
         while (!isExit) {
             if (getCommand()) {
