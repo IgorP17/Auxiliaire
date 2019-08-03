ba8fc14ce8a7007a4d24b10395300fb65d89e913
Игорь Прохоров <iprokhorov@mobicont.ru>
2019-08-03 19:22:07 +0300

    hard level

--- src/games/tictactoe/HardLevel.java
+++ src/games/tictactoe/HardLevel.java
@@ -1,4 +1,16 @@
 package games.tictactoe;
 
 public class HardLevel {
+
+    public static void main(String[] args) {
+        String huPlayer = "O";
+        String aiPlayer = "X";
+
+        // this is the board flattened and filled with some values to easier asses the Artificial Inteligence.
+        String[] origBoard = new String[]{"O", "1", "X", "X", "4", "X", "6", "O", "O"};
+        //keeps count of function calls
+        int fc = 0;
+
+    }
+
 }
