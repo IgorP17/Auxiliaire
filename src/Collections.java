96ef3636f3cf13c7afdda60d01e61e2b1b5507ae
Игорь Прохоров <iprokhorov@mobicont.ru>
2020-02-28 08:40:02 +0300

    flashcards

--- src/Collections.java
+++ src/Collections.java
@@ -17,13 +17,13 @@ public class Collections {
         // getting a mutable set from an immutable one
         Set<String> countries = new HashSet<>(List.of("India", "Japan", "Switzerland"));
         countries.addAll(List.of("India", "Germany", "Algeria"));
-        System.out.println(countries ); // [Japan, Algeria, Switzerland, Germany, India]
+        System.out.println(countries); // [Japan, Algeria, Switzerland, Germany, India]
 
         countries.retainAll(List.of("Italy", "Japan", "India", "Germany"));
-        System.out.println(countries ); // [Japan, Germany, India]
+        System.out.println(countries); // [Japan, Germany, India]
 
         countries.removeAll(List.of("Japan", "Germany", "USA"));
-        System.out.println(countries ); // [India]
+        System.out.println(countries); // [India]
 
         System.out.println(countries.containsAll(Set.of("India", "Japan")));   // true
 
@@ -34,5 +34,29 @@ public class Collections {
         Objects.equals(numbers, List.of(1, 2, 3)); // true
 
         Set<String> nameSet = new TreeSet<>(Arrays.asList("Mr.Green", "Mr.Yellow", "Mr.Red"));
+
+        // maps
+        Map<String, String> friendPhones = Map.of(
+                "Bob", "+1-202-555-0118",
+                "James", "+1-202-555-0220",
+                "Katy", "+1-202-555-0175"
+        );
+
+        // printing names
+        for (String name : friendPhones.keySet()) {
+            System.out.println(name);
+        }
+
+        // printing phones
+        for (String phone : friendPhones.values()) {
+            System.out.println(phone);
+        }
+
+        // key + value
+        for (var entry : friendPhones.entrySet()) {
+            System.out.println(entry.getKey() + ": " + entry.getValue());
+        }
+
+        friendPhones.forEach((name, phone) -> System.out.println(name + ": " + phone));
     }
 }
