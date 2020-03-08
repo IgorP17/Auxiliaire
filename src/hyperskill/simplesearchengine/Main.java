d42009e45e9a994773b1f6443a89bf1e3a1a4b41
Игорь Прохоров <iprokhorov@mobicont.ru>
2020-03-08 11:42:19 +0300

    search engine

--- src/hyperskill/simplesearchengine/Main.java
+++ src/hyperskill/simplesearchengine/Main.java
@@ -5,8 +5,11 @@ import java.util.Scanner;
 
 public class Main {
 
+    private static final String separator = System.lineSeparator();
+
     public static void main(String[] args) {
         ArrayList<People> peoples = new ArrayList<>();
+        ArrayList<People> found;
         Scanner scanner = new Scanner(System.in);
         System.out.println("Enter the number of people:");
         int num = Integer.parseInt(scanner.nextLine());
@@ -17,13 +20,35 @@ public class Main {
             peoples.add(People.parce(s));
         }
 
-        System.out.println("Enter the number of search queries:");
+        System.out.println(separator + "Enter the number of search queries:");
         int tryCount = Integer.parseInt(scanner.nextLine());
+        String search;
         for (int i = 0; i < tryCount; i++) {
-
+            System.out.println(separator + "Enter data to search people:");
+            search = scanner.nextLine();
+            found = findPeople(peoples, search);
+            if (found.isEmpty()) {
+                System.out.println(separator + "No matching people found.");
+            } else {
+                System.out.println(separator + "Found people:");
+                for (People p : found) {
+                    System.out.println(p);
+                }
+            }
         }
 
     }
+
+    private static ArrayList<People> findPeople(ArrayList<People> people, String find) {
+        ArrayList<People> result = new ArrayList<>();
+        for (People p : people) {
+            if (p.isContains(find)) {
+                result.add(p);
+            }
+        }
+        return result;
+    }
+
 }
 
 class People {
@@ -39,6 +64,7 @@ class People {
 
     /**
      * Parse [] to people
+     *
      * @param s - massive of strings
      * @return - People object
      */
@@ -46,9 +72,9 @@ class People {
         if (null == s || s.length == 0) {
             return new People(null, null, null);
         } else {
-            if (s.length == 3){
+            if (s.length == 3) {
                 return new People(s[0], s[1], s[2]);
-            } else if (s.length == 2){
+            } else if (s.length == 2) {
                 return new People(s[0], s[1], null);
             } else {
                 return new People(s[0], null, null);
@@ -59,13 +85,15 @@ class People {
 
     /**
      * If any field equals
+     *
      * @param s - search string
      * @return - if any equals
      */
-    boolean isContains(String s){
-        return (s.equalsIgnoreCase(firstName) ||
-                s.equalsIgnoreCase(secondName) ||
-                s.equalsIgnoreCase(email));
+    boolean isContains(String s) {
+        s = s.toLowerCase().trim();
+        return (firstName.toLowerCase().contains(s) ||
+                (null != secondName && secondName.toLowerCase().contains(s)) ||
+                (null != email && email.toLowerCase().contains(s)));
     }
 
     @Override
