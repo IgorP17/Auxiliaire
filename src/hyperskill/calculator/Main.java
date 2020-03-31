eabb1905f12bfff95b640404022af6a3a13a7741
Игорь Прохоров <iprokhorov@mobicont.ru>
2020-03-31 18:38:40 +0300

    calculator

--- src/hyperskill/calculator/Main.java
+++ src/hyperskill/calculator/Main.java
@@ -2,8 +2,7 @@ package hyperskill.calculator;
 
 import java.util.ArrayList;
 import java.util.Scanner;
-import java.util.regex.Matcher;
-import java.util.regex.Pattern;
+import java.util.Stack;
 
 public class Main {
 
@@ -14,83 +13,174 @@ public class Main {
         String s;
         while (!isExit) {
             s = scanner.nextLine();
-            switch (s) {
-                case "/exit":
-                    System.out.println("Bye!");
-                    isExit = true;
-                    break;
-                case "/help":
-                    help();
-                    break;
-                default:
-                    calculate(s);
-                    break;
-            }
-        }
+            if (s.startsWith("/")) {
+                switch (s) {
+                    case "/exit":
+                        System.out.println("Bye!");
+                        isExit = true;
+                        break;
+                    case "/help":
+                        help();
+                        break;
+                    default:
+                        System.out.println("Unknown command");
+                        break;
+                }
+            } else {
+                if (!s.isEmpty()) {
+                    ArrayList<String> reversed = getReversePolish(s);
+                    System.out.println(calcPolish(reversed));
+//                    for (String s1 : reversed){
+//                        System.out.print(s1 + " ");
+//                    }
 
-    }
-
-    private static void calculate(String all) {
-        String in = all.replaceAll(" ", "");
-        if (!(in.startsWith("+") || in.startsWith("-"))) {
-            in = "+" + in;
-        }
-//        System.out.println(in);
-        // split to args
-        Pattern pattern = Pattern.compile("[-+]+\\d+", Pattern.CASE_INSENSITIVE);
-        Matcher matcher = pattern.matcher(in);
-        ArrayList<String> args = new ArrayList<>();
-        while (matcher.find()) {
-//            System.out.println(matcher.group());
-            args.add(matcher.group());
-        }
-        int result = 0;
-        Integer c;
-        for (String s : args){
-            c = refine(s);
-            if (null == c){
-                System.out.println("Refine failed! Value = " + s);
-                System.exit(1);
-            }
-            result = result + c;
-        }
-        System.out.println(result);
-    }
-
-    /**
-     * Refine smth like "---5"
-     * @param s string to evaluate
-     * @return integer
-     */
-    private static Integer refine(String s) {
-//        System.out.println("Got " + s);
-        Pattern pattern = Pattern.compile("[-+]", Pattern.CASE_INSENSITIVE);
-        Matcher matcher = pattern.matcher(s);
-        String current = "+";
-        while (matcher.find()) {
-//            System.out.println(matcher.group());
-            if ("-".equalsIgnoreCase((matcher.group()))){
-                // need invert
-                if ("+".equalsIgnoreCase(current)){
-                    current = "-";
-                } else {
-                    current = "+";
                 }
             }
         }
-        pattern = Pattern.compile("\\d+", Pattern.CASE_INSENSITIVE);
-        matcher = pattern.matcher(s);
-        if (matcher.find()){
-            if ("+".equalsIgnoreCase(current)){
-                return Integer.parseInt(matcher.group());
+
+    }
+
+    /**
+     * Evaluate polish string
+     *
+     * @param in reversed polish string as ArrayList
+     * @return eval value
+     */
+    private static int calcPolish(ArrayList<String> in){
+        Stack<String> current = new Stack<>();
+
+        System.out.println("Got " + in);
+
+        for (String s : in){
+            System.out.println("Current = " + current.toString());
+            if (s.matches("\\d+")){
+                // we got an operand put in stack
+                current.push(s);
             } else {
-                return -Integer.parseInt(matcher.group());
+                // we got operator
+                // get right value
+                int right = Integer.parseInt(current.pop());
+                // get left value
+                int left = Integer.parseInt(current.pop());
+
+                switch (s){
+                    case "+":
+                        System.out.println("Have addition = " + (left + right));
+                        // need add result
+                        current.push(String.valueOf(left + right));
+                        break;
+                    case "-":
+                        System.out.println("Have substraction = " + (left - right));
+                        // need add result
+                        current.push(String.valueOf(left - right));
+                }
             }
         }
-        return null;
+        if (current.size() == 1){
+            return Integer.parseInt(current.pop());
+        } else {
+            throw new RuntimeException("Illegal result stack!");
+        }
+    }
+
+    /**
+     * Get reversed polish notation
+     * https://ru.wikipedia.org/wiki/%D0%9E%D0%B1%D1%80%D0%B0%D1%82%D0%BD%D0%B0%D1%8F_%D0%BF%D0%BE%D0%BB%D1%8C%D1%81%D0%BA%D0%B0%D1%8F_%D0%B7%D0%B0%D0%BF%D0%B8%D1%81%D1%8C
+     * WARNING use + and - sign // TODO implement others with priority
+     *
+     * @param in string like 3 + 4
+     * @return reversed polish notation string
+     */
+    private static ArrayList<String> getReversePolish(String in) {
+        in = "0 + " + in;
+        ArrayList<String> result = new ArrayList<>();
+        // reading string
+        // assume we have operators + - and operands
+        StringBuilder operand = new StringBuilder();
+        StringBuilder operator = new StringBuilder();
+        boolean isReadingOperand = false;
+        boolean isReadingOperator = false;
+        char ch;
+        Stack<String> stack = new Stack<>();
+
+        for (int i = 0; i < in.length(); i++) {
+            ch = in.charAt(i);
+            if (Character.isDigit(ch)) {
+                isReadingOperand = true;
+                operand.append(ch);
+
+                // if we return to read after operand
+                if (isReadingOperator) {
+                    // we finish read operator
+                    isReadingOperator = false;
+                    // add operator to Stack
+                    stack.push(refineOperator(operator.toString()));
+                    // flush operator
+                    operator = new StringBuilder();
+                }
+
+                // the last symbol should be a digit
+                if (i == in.length() - 1) {
+                    // add to out
+                    result.add(operand.toString());
+                }
+            } else {
+                if (isReadingOperand) {
+                    // we just finish reading operand
+                    isReadingOperand = false;
+                    // add operand to out
+                    result.add(operand.toString());
+                    // flush operand
+                    operand = new StringBuilder();
+                }
+                // if it is last symbol throw
+                if (i == in.length() - 1) {
+                    throw new RuntimeException("Last is not digit!");
+                }
+                // so we are reading operator
+                isReadingOperator = true;
+                operator.append(ch);
+            }
+        }
+        // print stack
+//        while (!stack.isEmpty()) {
+//            System.out.println(stack.pop());
+//        }
+
+        // put stack in
+        while (!stack.isEmpty()){
+            result.add(stack.pop());
+        }
+        return result;
     }
 
 
+    private static String refineOperator(String operator) {
+        boolean isPlusMinusFound = false;
+        char result = '+';
+        for (char c : operator.toCharArray()) {
+            if ('+' == c) {
+                isPlusMinusFound = true;
+            }
+
+            if ('-' == c) {
+                isPlusMinusFound = true;
+                if ('+' == result) {
+                    result = '-';
+                } else {
+                    result = '+';
+                }
+            }
+        }
+        if (!isPlusMinusFound) {
+            throw new RuntimeException("Bad operator");
+        }
+        return String.valueOf(result);
+    }
+
+    /**
+     * Print some help
+     */
     private static void help() {
         System.out.println("Enter line to calculate");
         System.out.println("For example 9 +++ 10 -- 8");
