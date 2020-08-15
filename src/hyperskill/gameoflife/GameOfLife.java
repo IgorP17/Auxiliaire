c21c6d2b33b052df15b68a549a2e9e052777f71e
Игорь Прохоров <iprokhorov@mobicont.ru>
2020-08-15 11:49:06 +0300

    game of live s4

---
+++ src/hyperskill/gameoflife/GameOfLife.java
@@ -0,0 +1,14 @@
+package hyperskill.gameoflife;
+
+import javax.swing.*;
+
+public class GameOfLife extends JFrame {
+    // For the testing reasons, you need to set the name of each component using the method component.setName(String name).
+    // Name your labels with names "GenerationLabel" and "AliveLabel".
+
+    public GameOfLife() {
+        setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        setSize(300, 300);
+        setVisible(true);
+    }
+}
