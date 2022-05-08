9e4e012193cd1ce59e1907aabddd314b58ace32d
Игорь Прохоров <iprokhorov@mobicont.ru>
2022-05-08 16:09:23 +0300

    udemy

---
+++ src/udemy/Udemy1.java
@@ -0,0 +1,14 @@
+package udemy;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class Udemy1 {
+
+    public static void main(String[] args) throws IOException {
+//        BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
+//        System.out.println(bufferedReader.readLine());
+
+    }
+}
