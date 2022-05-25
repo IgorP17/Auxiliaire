e61534b61965e9f841c729850dd955e127b4cd25
Игорь Прохоров <iprokhorov@mobicont.ru>
2022-05-25 11:55:56 +0300

    move

--- test/udemy/UdemyTest.java
+++ test/udemy/UdemyTest.java
@@ -4,6 +4,7 @@ import org.junit.Assert;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.junit.runners.Parameterized;
+import udemy.core1.Udemy;
 
 import java.util.Arrays;
 import java.util.Collection;
