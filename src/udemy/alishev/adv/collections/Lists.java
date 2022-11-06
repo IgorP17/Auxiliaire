044a1a4cf233f2d2408c5dcf7e2dc81fdbc0f4bd
Игорь Прохоров <iprokhorovster@gmail.com>
2022-11-06 14:38:28 +0300

    a1

---
+++ src/udemy/alishev/adv/collections/Lists.java
@@ -0,0 +1,11 @@
+package udemy.alishev.adv.collections;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Lists {
+
+    public static void main(String[] args) {
+        List<Integer> arrayList = new ArrayList<>();
+    }
+}
