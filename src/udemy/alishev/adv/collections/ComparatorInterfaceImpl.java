94cdcba5e13a949c078bca839deb73668fd49c57
Игорь Прохоров <iprokhorovster@gmail.com>
2023-02-25 09:56:17 +0300

    nail adv - finish lesson 012 collection

---
+++ src/udemy/alishev/adv/collections/ComparatorInterfaceImpl.java
@@ -0,0 +1,85 @@
+package udemy.alishev.adv.collections;
+
+import java.util.*;
+
+public class ComparatorInterfaceImpl {
+    public static void main(String[] args) {
+        List<String> list = new ArrayList<>();
+        list.add("frog");
+        list.add("z");
+        list.add("alligator");
+        list.add("bird");
+        list.add("dog");
+        list.add("cat");
+
+//        Collections.sort(list);
+//        Collections.sort(list, Collections.reverseOrder());
+        Collections.sort(list, new StringLengthComparator());
+        System.out.println(list);
+
+        List<Integer> iList = new ArrayList<>();
+        iList.add(10);
+        iList.add(500);
+        iList.add(0);
+        iList.add(250);
+
+        // Сортировка через анонимный класс
+        Collections.sort(iList, new Comparator<Integer>() {
+            @Override
+            public int compare(Integer integer, Integer t1) {
+                if (integer > t1)
+                    return -1;
+                else if (integer < t1)
+                    return 1;
+                else
+                    return 0;
+            }
+        });
+        System.out.println(iList);
+
+        // Сортировка объектов
+        Person person3 = new Person(3, "Mike");
+        Person person1 = new Person(1, "Bob");
+        Person person2 = new Person(2, "Katy");
+        List<Person> people = new ArrayList<>();
+        people.add(person1);
+        people.add(person2);
+        people.add(person3);
+
+        Collections.sort(people, new Comparator<Person>() {
+            @Override
+            public int compare(Person o1, Person o2) {
+                if (o1.getId() > o2.getId())
+                    return 1;
+                else if (o1.getId() < o2.getId())
+                    return -1;
+                else
+                    return 0;
+            }
+        });
+
+        System.out.println(people);
+
+    }
+}
+
+class StringLengthComparator implements Comparator<String> {
+
+    @Override
+    public int compare(String o1, String o2) {
+        /*
+            o1 > o2 -> 1
+            o1 < o2 -> -1
+            o1 == o2 -> 0
+         */
+        if (o1.length() > o2.length()) {
+            return 1;
+        } else if (o1.length() < o2.length()) {
+            return -1;
+        } else {
+            return 0;
+        }
+    }
+}
+
+
---
+++ src/udemy/alishev/adv/collections/Person.java
@@ -0,0 +1,57 @@
+package udemy.alishev.adv.collections;
+
+import java.util.Objects;
+
+public class Person {
+    private final int id;
+    private final String name;
+
+    public Person(int id, String name) {
+        this.id = id;
+        this.name = name;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    @Override
+    public String toString() {
+        return "Person{" +
+                "id=" + id +
+                ", name='" + name + '\'' +
+                '}';
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+
+        Person person = (Person) o;
+
+        if (id != person.id) return false;
+        return Objects.equals(name, person.name);
+    }
+
+    // {person} -> int(!!фиксированная длина) с учетом полей
+    // может случиться так, что хеш коды 2-х разных объектов равны - коллизия, поэтому нужен equals
+    @Override
+    public int hashCode() {
+        int result = id;
+        result = 31 * result + (name != null ? name.hashCode() : 0);
+        return result;
+    }
+
+    /*
+     * Контракт hashcode и equals
+     * 1) У двух проверяемых объектов вызываем hashCode.
+     * Если получили 2 разных числа - объекты точно разные.
+     * 2) если hashCode одинаков, то вызываем equals:
+     * который даст ответ - одинаковы объекты или нет.
+     */
+}
